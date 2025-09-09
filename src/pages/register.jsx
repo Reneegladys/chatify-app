11bd7c06d30519faeacfbf6b74af17866a6074a8
Renée Gladys <renee16@live.se>
2025-09-09 13:42:11 +0200

    Fix: corrected CSS import case for Netlify build

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Register.css"; // Import the CSS file
+import "./register.css"; // Import the CSS file
 
 const BASE_URL = "https://chatify-api.up.railway.app";
 
